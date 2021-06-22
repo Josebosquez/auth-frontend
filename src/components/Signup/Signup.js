94414ee339d87bed408a8871398a65d379c59ed2
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-06-21 23:18:32 -0600

    initial commit with some comments

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -8,8 +8,8 @@ export class Signup extends Component {
     username: "",
     email: "",
     password: "",
-    confirmPassword: "",
-    firstNameError: "",
+    confirmPassword: "",  // the variables above are the blank state that hold the input box value
+    firstNameError: "",   // the variables below are the blank errors that appear when the if statements are met.
     lastNameError: "",
     usernameError: "",
     emailError: "",
@@ -38,7 +38,7 @@ export class Signup extends Component {
 
   handleOnBlur = (event) => {
     console.log(event.target.name);
-    console.log("Hanlde onBlur Triggered");
+    console.log("Handle onBlur Triggered");
 
     if (this.state[event.target.name].length === 0) {
       this.setState({
@@ -49,7 +49,7 @@ export class Signup extends Component {
 
   render() {
     const {
-      firstName,
+      firstName, // brings in the variables from the state to use in our html. these will be assigned as the values to our input box. Err variables will appear based on onblur func.
       lastName,
       username,
       email,
@@ -72,18 +72,18 @@ export class Signup extends Component {
             <div className="form-group-inline">
               <div className="inline-container">
                 <label htmlFor="firstName">First Name</label>
-                <input
-                  type="text"
-                  id="firstName"
-                  value={firstName}
-                  placeholder="First Name"
-                  name="firstName"
-                  onChange={this.handleOnChange}
-                  autoFocus
-                  onBlur={this.handleOnBlur}
+                <input // the input syntax has the following values that act like normal html without it being all fancy..  
+                  type="text" // accepts text
+                  id="firstName" // id name for css
+                  value={firstName} // value is what is being stored in the input box
+                  placeholder="First Name" // the words that appear in the input box before you start typing.
+                  name="firstName" // this is the name.
+                  onChange={this.handleOnChange}  // handles the onchange function on line 20. 
+                  autoFocus // when page renders, user will be able to automatically type into the box that this autofocus is attached to.
+                  onBlur={this.handleOnBlur} // handles the on blur function on line 39. what it does is it acts like our err msg checker. if there are 0 characters in length of the input, then it will make the error msg div on line 85 appear in red. 
                 />
                 <div className="errorMessage">
-                  {firstNameError && firstNameError}
+                  {firstNameError && firstNameError /* this will render if the conditions in the onblur function are met. */ } 
                 </div>
               </div>
 
@@ -174,8 +174,8 @@ export class Signup extends Component {
               </div>
             </div>
 
-            <div className="button-container">
-              <button>Submit</button>
+            <div className="button-container" /* this is the submit button, will work on it tomorrow.*/>
+              <button>Submit</button>  
             </div>
           </form>
         </div>
